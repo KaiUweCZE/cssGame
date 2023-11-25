96bcb553cb22fc3198a3ccea6dbb74a391336b09
Kai Uwe <pisezdenekpise@seznam.cz>
2023-11-25 17:18:32 +0100

    initial initialization + styling CssForm component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CssForm from "./components/CssForm";
+
+
+const App = () => {
+
+    return(
+        <>
+            <h2>Welcome</h2>
+            <CssForm />
+        </>
+    )
+}
+
+export default App
