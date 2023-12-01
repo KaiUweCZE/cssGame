82510e40a2662d6ed43c6361e8e5cde9a40b5052
KaiUweCZE <pisezdenekpise@seznam.cz>
2023-12-01 19:00:44 +0100

    created selection based on user's input

--- src/components/CssForm.js
+++ src/components/CssForm.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import leftBracket from '../styles/images/left-bracket.svg'
 import rightBracket from '../styles/images/right-bracket.svg'
-import playIcon from '../styles/images/play.svg'
+import playIcon from '../styles/images/play2.svg'
 import { list } from "../data/listOfProperities";
 import ErrorMessage from "./ErrorMessage";
+import SuggestList from "./SuggestList";
 
 const CssForm = () => {
     const [property, setProperty] = useState("");
@@ -35,11 +36,12 @@ const CssForm = () => {
             <span className="element-class">.bridge</span>
             <img className="left-bracket" src={leftBracket} alt="" />
             <form className="element-class__bridge" action="">
-                <input type="text" name="" id="" onChange={(e) => setProperty(e.target.value)}/>
+                <input type="text" value={property} name="" id="" onChange={(e) => setProperty(e.target.value)}/>
                 <input type="text" name="" id="" onChange={(e) => setPropertyValue(e.target.value)}/>
                 <button className="play" onClick={(e) => checkResult(e)}><img src={playIcon} alt="" /></button>
             </form>
             <img className="right-bracket" src={rightBracket} alt="" />
+            <SuggestList value={property} func={setProperty} />
             {
                 error ? <ErrorMessage text={errotMessage}/> : ""
             }
---
+++ src/components/SuggestList.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { list } from "../data/listOfProperities";
+
+
+const SuggestList = (props) => {
+    const [suggestions, setSuggestions] = useState([]);
+    const [visible, setVisible] = useState(false)
+
+    useEffect(()=> {
+        const findSimilar = () =>{
+            if (props.value !== "") {
+                const filteredArray = list.filter(prop => prop.startsWith(props.value) && prop !== props.value)
+                setSuggestions(filteredArray)
+                if (filteredArray.length < 1) {
+                    setVisible(false)
+                } else {
+                    setVisible(true)
+                }    
+            }
+            if (props.value === "") {
+                setVisible(false)
+            }
+        }
+        findSimilar()
+        
+    }, [props.value])
+
+    const confrimText = (e) => {
+        setVisible(false)
+        props.func(e)
+    }
+
+    return(
+        visible ?
+            <ul className="box-colors">
+            {
+                suggestions.map((property, index) => {
+
+                    return(
+                        <li key={index} onClick={e => confrimText(e.target.outerText)}>{property}</li>
+                    )
+                })
+            }
+            </ul>
+            : ""
+    )
+}
+
+
+export default SuggestList
