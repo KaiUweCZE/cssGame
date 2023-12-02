a512e500b085473ec10b550021835a5142fadebf
KaiUweCZE <pisezdenekpise@seznam.cz>
2023-12-02 17:32:26 +0100

    auxiliary element for checking the correct positioning of the bridge

---
+++ src/components/CheckPoint.js
@@ -0,0 +1,13 @@
+import React, {forwardRef} from "react";
+
+const CheckPoint = forwardRef((props, ref) => {
+
+    return(
+        <div ref={ref} className="target-area">
+
+        </div>
+    )
+})
+
+
+export default CheckPoint
