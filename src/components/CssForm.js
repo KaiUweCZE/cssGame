b1ea84c91e9ed5ed16506cb58da64b5ea5f46cae
KaiUweCZE <pisezdenekpise@seznam.cz>
2023-12-02 17:35:01 +0100

    set up funcionalities for main trigger(CssForm)- primarily set to work with context

--- src/components/CssForm.js
+++ src/components/CssForm.js
@@ -1,33 +1,41 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import leftBracket from '../styles/images/left-bracket.svg'
 import rightBracket from '../styles/images/right-bracket.svg'
-import playIcon from '../styles/images/play2.svg'
+import playIcon from '../styles/images/play2a.webp'
+import playIconaAfter from '../styles/images/play2b.webp'
 import { list } from "../data/listOfProperities";
 import ErrorMessage from "./ErrorMessage";
 import SuggestList from "./SuggestList";
+import { EmojiContext, StyleContext } from "../contexts/FormContext";
 
 const CssForm = () => {
-    const [property, setProperty] = useState("");
-    const [propertyValue, setPropertyValue] = useState("");
+    const {property, setProperty, setPropertyValue} = useContext(StyleContext);
+    const {position, setPosition, setSpecialClass} = useContext(EmojiContext)
     const [error, setError] = useState(false);
     const [errotMessage, setErrorMessage] = useState("")
+    const [icon, setIcon] = useState(playIcon)
+    const [value, setValue] = useState("")
+
     
-    const checkTypo = (e) => {
-        if (list.includes(e)) {
-            setProperty(e);
+    const checkTypo = (input) => {
+        if (list.includes(input)) {
+            setProperty(input);
             setError(false)
+            setSpecialClass('run')
+            setPosition('92vw')
         } else{
             setProperty("")
             setError(true)
             setErrorMessage("Oh man, we do not know this property in thi universum!");
         }
     }
+
     
     const checkResult = (e) =>{
         e.preventDefault();
         checkTypo(property)
         if (!error) {
-            console.log(property, propertyValue);
+            setPropertyValue(value)
         }  
     }
 
@@ -37,8 +45,8 @@ const CssForm = () => {
             <img className="left-bracket" src={leftBracket} alt="" />
             <form className="element-class__bridge" action="">
                 <input type="text" value={property} name="" id="" onChange={(e) => setProperty(e.target.value)}/>
-                <input type="text" name="" id="" onChange={(e) => setPropertyValue(e.target.value)}/>
-                <button className="play" onClick={(e) => checkResult(e)}><img src={playIcon} alt="" /></button>
+                <input type="text" name="" id="" onChange={(e) => setValue(e.target.value)}/>
+                <button className="play" onMouseLeave={() => setIcon(playIcon)} onMouseEnter={() => setIcon(playIconaAfter)} onClick={(e) => checkResult(e)}><img src={icon} alt="" /></button>
             </form>
             <img className="right-bracket" src={rightBracket} alt="" />
             <SuggestList value={property} func={setProperty} />
