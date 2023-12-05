72a9ff845e123ca0c20a485dbe501520dc2f1bbf
KaiUweCZE <pisezdenekpise@seznam.cz>
2023-12-05 15:16:50 +0100

    editing styles and added About page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import HamburgerMenu from "./HamburgerMenu";
 
 const Navbar = () => {
+    const [isActive, setIsActive] = useState(false)
+
+
+    const specialClass = isActive ? "active" : ""
 
     return(
         <header className="container-menu">
             <nav className="navigation">
-                <ul className="menu">
-                    <NavLink to="/home">Home</NavLink>
-                    <NavLink to="/about">About</NavLink>
-                    <NavLink to="/game">Game</NavLink>
+                <HamburgerMenu state={isActive} setState={setIsActive}/>
+                <ul className={`menu ${specialClass}`}>
+                    <NavLink className="menu__item" to="/">Home</NavLink>
+                    <NavLink className="menu__item" to="about">About</NavLink>
+                    <NavLink className="menu__item" to="game">Game</NavLink>
                 </ul>
             </nav>
         </header>
