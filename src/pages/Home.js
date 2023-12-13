63d2a73d34c1366e45df00025ae90733c722aafd
Kai Uwe <pisezdenekpise@seznam.cz>
2023-12-13 18:20:15 +0100

    text added to Home, marginal editing of style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,24 @@ import React from "react";
 
 const Home = () => {
 
-    return
+    return(
+        <main className="container-home">
+            <section>
+                <h2>Verze 0</h2>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                    Iste tenetur eligendi vitae inventore cupiditate excepturi odio laudantium ad, placeat consequatur ea, delectus et id, voluptatum animi fugit laborum nesciunt obcaecati!
+                    Ab, eligendi ipsam. Quae, explicabo. Iste, repellat! 
+                    Corrupti nemo quos blanditiis exercitationem quae tempora 
+                    repellat hic sequi odit suscipit, a ipsum vero, consectetur quisquam ipsa nisi? Quisquam aut ut dolor.
+                    Centrem webu je css game, která má umožnit interaktivní stylování, za jehož pomoci uživatel bude překonávat překážky na cestě.
+                    V průběhu putování bude uživatel odměňován(tipy, videi, články, užitečné generátory, atd.),které
+                    by mu měly pomoci dostat se až na samí konec cesty, rozhodně v tom ale nebude sám,
+                    kdykoliv mu bude po ruce nápomocen průvodce....  
+                </p>
+            </section>
+        </main>
+    )
 }
 
 
