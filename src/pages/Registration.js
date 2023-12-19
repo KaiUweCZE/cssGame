1db182072e771e7b5a4c5ef5f47b00f68b9793ad
Kai Uwe <pisezdenekpise@seznam.cz>
2023-12-19 16:18:03 +0100

    styling sigin form 0.0.1

---
+++ src/pages/Registration.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+const Registration = () => {
+
+    return(
+        <div className="wrapper">
+            <div className="container-registration">
+                <div className="registration__info"></div>
+                <form className="registration__form" action="">
+                    <input type="text" />
+                    <input type="text" />
+                    <input type="submit" value="Sign in" />
+                </form>
+            </div>
+        </div>
+        
+        
+    )
+}
+
+export default Registration
