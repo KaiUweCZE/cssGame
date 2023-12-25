0847015d3d60c7680ab89d8e49682efc70758dd8
KaiUweCZE <pisezdenekpise@seznam.cz>
2023-12-25 13:08:17 +0100

    creating Obstacle. It will be used at some levels

---
+++ src/components/Obstacle.js
@@ -0,0 +1,10 @@
+import React from "react";
+import stoneImg from "../styles/images/stone.webp"
+
+const Obstacle = () => {
+
+    return <img className="obstacle" src={stoneImg} alt="" />
+
+}
+
+export default Obstacle
