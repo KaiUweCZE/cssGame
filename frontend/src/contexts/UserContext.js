99b5ab83fd3b72ee9046325366af2e184bd8cd70
KaiUweCZE <pisezdenekpise@seznam.cz>
2024-01-31 11:06:44 +0100

    setting modifications in contexts, setUser is implemented in useLevelUp hook and data are sending to localStoreg and databse + data are getting from localStorage(token) and databse(user.level)

--- frontend/src/contexts/UserContext.js
+++ frontend/src/contexts/UserContext.js
@@ -1,23 +1,48 @@
 import React, { createContext, useEffect, useState } from "react";
+import { gql, useQuery } from '@apollo/client'
+
+
+const GET_USER = gql`
+    query user($id: ID!){
+        user(id: $id){
+            id
+            name
+            email
+            level
+        }
+    }
+`
 
 export const UserContext = createContext({
     login: false,
     token: null,
+    user: '',
+    setUser: () => {},
+    level: 0,
     setLogin: () => {},
     setToken: () => {}
 })
 
 export const UserContextProvider = ({children}) => {
+    const [userId, setUserId] = useState(null);
     const [login, setLogin] = useState(false)
     const [token, setToken] = useState(null)
+    const [user, setUser] = useState({})
+    /*const { data, loading: queryLoading, error: queryError } = useQuery(GET_USER, {
+        variables: { id: userId },
+        skip: !userId, // Dotaz se neprovede, pokud není userId nastavené
+    });*/
     
     useEffect(() => {
         const storedToken = localStorage.getItem('authToken')
+        const storedUser = localStorage.getItem('user')
         if (storedToken) {
+            setUser(JSON.parse(storedUser))
             setToken(storedToken)
             setLogin(true)
         }
     }, [])
+
 
     const logout = () => {
         localStorage.removeItem('authToken')
@@ -28,6 +53,8 @@ export const UserContextProvider = ({children}) => {
     const contextValue ={
         login,
         token,
+        user,
+        setUser,
         setLogin,
         setToken,
         logout
