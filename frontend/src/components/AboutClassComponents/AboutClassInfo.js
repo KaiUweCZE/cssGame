4e53ff5ebb92755aea9d8823afaa7b5b2861e7b9
KaiUweCZE <pisezdenekpise@seznam.cz>
2024-02-05 12:13:32 +0100

    Creating styleChecker function(some levels need right position and right css values), imported to cssForm. AboutClassInfo has index for mapped element

--- frontend/src/components/AboutClassComponents/AboutClassInfo.js
+++ frontend/src/components/AboutClassComponents/AboutClassInfo.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 const AboutClassInfo = (props) => {
-
+    let index = 0
     return(
         <article className="about-class__article">
             {Object.entries(props.information).map(([key, value]) => {
+                index += 1
                 return(
-                    <li>{key}: {value}</li>
+                    <li key={index}>{key}: {value}</li>
                 )
                 })}
         </article>
