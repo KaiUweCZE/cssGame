4bdc33e04487b47cd9f00398e9b9bac8c5f7c688
Kai Uwe <pisezdenekpise@seznam.cz>
2024-02-10 13:41:36 +0100

    styling road map (added background)

--- frontend/src/pages/RoadMap.js
+++ frontend/src/pages/RoadMap.js
@@ -6,6 +6,7 @@ import parallaxBg from '../styles/images/parallax-background/sky.webp'
 import cloud1 from '../styles/images/parallax-background/cloud1.webp'
 import cloud2 from '../styles/images/parallax-background/cloud2.webp'
 import cloud3 from '../styles/images/parallax-background/cloud3.webp'
+import mapBG from '../styles/images/map.webp'
 import { UserContext } from "../contexts/UserContext";
 import RoadmapLevel from "../components/RoadmapLevel";
 import { LevelContext } from "../contexts/FormContext";
@@ -32,30 +33,35 @@ const RoadMap = () => {
     }
 
     return(
-        <div className="roadmap">
-            <RoadmapLevel onClick={() => setUpLevel(1)} specialClass='one' num={1} level={level} locked={ level < 0 ? true : false }/>
-            <RoadmapLevel onClick={() => setUpLevel(2)} specialClass='two' num={2} level={level} locked={ level < 1 ? true : false }/>
-            <RoadmapLevel onClick={() => setUpLevel(3)} specialClass='three' num={3} level={level} locked={ level < 2 ? true : false }/>
-            <RoadmapLevel onClick={() => setUpLevel(4)} specialClass='four' num={4} level={level} locked={ level < 3 ? true : false }/>
-            <RoadmapLevel onClick={() => setUpLevel(5)} specialClass='five' num={5} level={level} locked={ level < 4 ? true : false }/>
-            <RoadmapLevel onClick={() => setUpLevel(6)} specialClass='six' num={6} level={level} locked={ level < 5 ? true : false }/>
-            <RoadmapLevel onClick={() => setUpLevel(7)} specialClass='seven' num={7} level={level} locked={ level < 6 ? true : false }/>
-            <RoadmapLevel onClick={() => setUpLevel(8)} specialClass='eight' num={8} level={level} locked={ level < 7 ? true : false }/>
-            <RoadmapLevel onClick={() => setUpLevel(9)} specialClass='nine' num={9} level={level} locked={ level < 8 ? true : false }/>
-            <RoadmapLevel onClick={() => setUpLevel(10)} specialClass='ten' num={10} level={level} locked={ level < 9 ? true : false }/>
-            <RoadmapLevel onClick={() => setUpLevel(11)} specialClass='eleven' num={11} level={level} locked={ level < 10 ? true : false }/>
-            <RoadmapLevel onClick={() => setUpLevel(12)} specialClass='twelve' num={12} level={level} locked={ level < 11 ? true : false }/>
-            <RoadmapLevel onClick={() => setUpLevel(13)} specialClass='thirteen' num={13} level={level} locked={ level < 12 ? true : false }/>
-            <RoadmapLevel onClick={() => setUpLevel(14)} specialClass='fourteen' num={14} level={level} locked={ level < 13 ? true : false }/>
-            <RoadmapLevel onClick={() => setUpLevel(15)} specialClass='fifteen' num={15} level={level} locked={ level < 14 ? true : false }/>
-            <RoadmapLevel onClick={() => setUpLevel(16)} specialClass='sixteen' num={16} level={level} locked={ level < 15 ? true : false }/>
-            <RoadmapLevel onClick={() => setUpLevel(17)} specialClass='seventeen' num={17} level={level} locked={ level < 16 ? true : false }/>
-            <RoadmapLevel onClick={() => setUpLevel(18)} specialClass='eighteen' num={18} level={level} locked={ level < 17 ? true : false }/>
-            <RoadmapLevel onClick={() => setUpLevel(19)} specialClass='nineteen' num={19} level={level} locked={ level < 18 ? true : false }/>
-            <RoadmapLevel onClick={() => setUpLevel(20)} specialClass='twenty' num={20} level={level} locked={ level < 19 ? true : false }/>
+        <div className="roadmap__background" style={{backgroundImage: `url(${mapBG})`}}>
+            <div className="roadmap">
+                <RoadmapLevel onClick={() => setUpLevel(1)} specialClass='one' num={1} level={level} locked={ level < 0 ? true : false }/>
+                <RoadmapLevel onClick={() => setUpLevel(2)} specialClass='two' num={2} level={level} locked={ level < 1 ? true : false }/>
+                <RoadmapLevel onClick={() => setUpLevel(3)} specialClass='three' num={3} level={level} locked={ level < 2 ? true : false }/>
+                <RoadmapLevel onClick={() => setUpLevel(4)} specialClass='four' num={4} level={level} locked={ level < 3 ? true : false }/>
+                <RoadmapLevel onClick={() => setUpLevel(5)} specialClass='five' num={5} level={level} locked={ level < 4 ? true : false }/>
+                <RoadmapLevel onClick={() => setUpLevel(6)} specialClass='six' num={6} level={level} locked={ level < 5 ? true : false }/>
+                <RoadmapLevel onClick={() => setUpLevel(7)} specialClass='seven' num={7} level={level} locked={ level < 6 ? true : false }/>
+                <RoadmapLevel onClick={() => setUpLevel(8)} specialClass='eight' num={8} level={level} locked={ level < 7 ? true : false }/>
+                <RoadmapLevel onClick={() => setUpLevel(9)} specialClass='nine' num={9} level={level} locked={ level < 8 ? true : false }/>
+                <RoadmapLevel onClick={() => setUpLevel(10)} specialClass='ten' num={10} level={level} locked={ level < 9 ? true : false }/>
+                <RoadmapLevel onClick={() => setUpLevel(11)} specialClass='eleven' num={11} level={level} locked={ level < 10 ? true : false }/>
+                <RoadmapLevel onClick={() => setUpLevel(12)} specialClass='twelve' num={12} level={level} locked={ level < 11 ? true : false }/>
+                <RoadmapLevel onClick={() => setUpLevel(13)} specialClass='thirteen' num={13} level={level} locked={ level < 12 ? true : false }/>
+                <RoadmapLevel onClick={() => setUpLevel(14)} specialClass='fourteen' num={14} level={level} locked={ level < 13 ? true : false }/>
+                <RoadmapLevel onClick={() => setUpLevel(15)} specialClass='fifteen' num={15} level={level} locked={ level < 14 ? true : false }/>
+                <RoadmapLevel onClick={() => setUpLevel(16)} specialClass='sixteen' num={16} level={level} locked={ level < 15 ? true : false }/>
+                <RoadmapLevel onClick={() => setUpLevel(17)} specialClass='seventeen' num={17} level={level} locked={ level < 16 ? true : false }/>
+                <RoadmapLevel onClick={() => setUpLevel(18)} specialClass='eighteen' num={18} level={level} locked={ level < 17 ? true : false }/>
+                <RoadmapLevel onClick={() => setUpLevel(19)} specialClass='nineteen' num={19} level={level} locked={ level < 18 ? true : false }/>
+                <RoadmapLevel onClick={() => setUpLevel(20)} specialClass='twenty' num={20} level={level} locked={ level < 19 ? true : false }/>
+            </div>
         </div>
     )
 }
 
 
 export default RoadMap
+
+
+               
