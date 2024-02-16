a5406f081b5519f2da8c503b86c28a5d049ddeba
Kai Uwe <pisezdenekpise@seznam.cz>
2024-02-16 20:42:12 +0100

    initialize guide emoji, welcome page wich will serve asintro to game

---
+++ frontend/src/components/GuideEmoji.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import guideImg from '../styles/images/guide-emoji.webp'
+
+const GuideEmoji = () => {
+
+  return (
+    <img src={guideImg} alt="" className={`emoji-character`}/>
+  )
+}
+
+export default GuideEmoji
---
+++ frontend/src/pages/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// this page will serve as an intro to the game
+// and add story line
+const Welcome = () => {
+  return (
+    <div className="container-mission">
+        
+    </div>
+  )
+}
+
+export default Welcome
