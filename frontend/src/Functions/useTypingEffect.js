a672aada0b42c510aded68bdb7725105fdd65970
Kai Uwe <pisezdenekpise@seznam.cz>
2024-02-17 18:43:14 +0100

    customHook was created, useTypingEffect obtains a text and milliseconds and performs the typewriter effect

---
+++ frontend/src/Functions/useTypingEffect.js
@@ -0,0 +1,23 @@
+import {useEffect, useState} from "react";
+
+export const useTypingEffect = (text, miliseconds) => {
+    const [position, setPosition] = useState(0)
+    let i = 0
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setPosition((value) => value + 1)
+            i++;
+            if (i > text.length) {
+                clearInterval(intervalId)
+            }
+        }, miliseconds);
+        return () => {
+            clearInterval(intervalId)
+            i = 0
+            setPosition(0)
+        }
+       
+    }, [miliseconds, text.length, text])
+
+    return text.substring(0, position)
+}
