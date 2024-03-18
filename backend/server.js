defef82cfae703ec2dbaf37b98282bf8c14c194f
KaiUweCZE <pisezdenekpise@seznam.cz>
2024-03-18 13:30:56 +0100

    vercel.json is set

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-import cors from 'cors'
-import dotenv from 'dotenv'
-import express from 'express'
-import { graphqlHTTP } from 'express-graphql';
-import { connectDB } from './db.js';
-import { schema } from './schema.js';
-import { authMiddleware, handleLogin } from './auth.js';
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { connectDB } from "./db.js";
+import { schema } from "./schema.js";
+import { authMiddleware, handleLogin } from "./auth.js";
 //import helmet
 //import rateLimit
 //import { readFile } from 'node:fs/promises';
@@ -20,20 +20,27 @@ const PORT = process.env.PORT || 9001;
 // connection to the database
 connectDB();
 
-app.use(cors())
+app.use(cors());
 
-app.use(authMiddleware)
-app.post('/login', handleLogin);
+app.use(authMiddleware);
+app.post("/login", handleLogin);
 
 // could be useful
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === 'development'
-}))
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
-app.listen( {port: PORT}, () => {
+/*app.listen( {port: PORT}, () => {
     console.log(`Server is running on port: ${PORT}`);
-})
+})*/
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port: ${process.env.PORT}`);
+});
