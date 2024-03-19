5282c52eef9b0bdc793aa59f29d3972836c633d5
KaiUweCZE <pisezdenekpise@seznam.cz>
2024-03-19 19:17:18 +0100

    redirects is set, CopyWebpackPlugin was installed and has been imported to webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,12 +1,13 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js',
-    publicPath: "/"
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+    publicPath: "/",
   },
   module: {
     rules: [
@@ -14,25 +15,28 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif|webp)$/,
-        use: ['file-loader']
-      }
-    ]
+        use: ["file-loader"],
+      },
+    ],
   },
   devServer: {
     historyApiFallback: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
+      template: "./src/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "src/_redirects", to: "" }],
+    }),
+  ],
 };
