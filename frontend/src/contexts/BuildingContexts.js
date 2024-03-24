09d674a70fdd6d0aa32bc06721e1c6330296a65d
KaiUweCZE <pisezdenekpise@seznam.cz>
2024-03-24 17:21:44 +0100

    a RestrictionContext is created that accepts list of allowed properties / list of denied properties

--- frontend/src/contexts/BuildingContexts.js
+++ frontend/src/contexts/BuildingContexts.js
@@ -84,11 +84,44 @@ export const RestrictionProvider = ({ children }) => {
   const [allowedList, setAllowedList] = useState([""]);
   const [deniedList, setDeniedList] = useState([""]);
 
+  const handleAdd = (type, number, setNumber) => {
+    //const list = type === "allowed" ? allowedList : deniedList;
+    const setList = type === "allowed" ? setAllowedList : setDeniedList;
+    setNumber(number + 1);
+    setList((prev) => [...prev, ""]);
+  };
+
+  const handleRemove = (type, number, setNumber) => {
+    const list = type === "allowed" ? allowedList : deniedList;
+    const setList = type === "allowed" ? setAllowedList : setDeniedList;
+    if (number > 0) {
+      setNumber(number - 1);
+      const newList = list.slice(0, -1);
+      setList(newList);
+    }
+  };
+
+  const handleListChange = (index, value, type) => {
+    const list = type === "allowed" ? allowedList : deniedList;
+    const setList = type === "allowed" ? setAllowedList : setDeniedList;
+    const newList = list.map((item, i) => (i === index ? value : item));
+    setList(newList);
+  };
+
+  const logList = (type) => {
+    const list = type === "allowed" ? allowedList : deniedList;
+    console.log(list);
+  };
+
   const contextValues = {
     allowedList,
     deniedList,
     setAllowedList,
     setDeniedList,
+    handleAdd,
+    handleRemove,
+    handleListChange,
+    logList,
   };
 
   return (
