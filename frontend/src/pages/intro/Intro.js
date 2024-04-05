4c37f69acafbf1bd784109d2b02e8ef2b8da088a
Kai Uwe <pisezdenekpise@seznam.cz>
2024-04-05 05:33:40 +0200

    change intro for newcomers, styling around AsideBox

--- frontend/src/pages/intro/Intro.js
+++ frontend/src/pages/intro/Intro.js
@@ -1,42 +1,33 @@
 import React, { useEffect, useState } from "react";
-import "@styles/intro-styles.css";
-import { enviromentImages, guideImg, masterImg } from "../../data/ImagesData";
-import GuideEmoji from "@components/emojis/GuideEmoji";
-import MainEmoji from "@components/emojis/MainEmoji";
-import FirstScene from "@pages/intro/intro-components/FirstScene";
-import SecondScene from "./intro-components/SecondScene";
-import ThirdScene from "./intro-components/ThirdScene";
-import { useNavigate } from "react-router-dom";
+import "./new-styles.css";
+import IntroSlide from "./IntroSlide";
+import firstImg from "@images/firstScene.webp";
+import { slideData } from "./data/slideData";
+import Loader from "@components/Loader";
 
-// this page will serve as an intro to the game
-// and add story line
-const Welcome = () => {
-  const [scene, setScene] = useState("first");
-  const navigate = useNavigate();
+const Intro = () => {
+  const [slideId, setSlideId] = useState(1);
+  const data = slideData.find((e) => e.id === slideId);
 
   useEffect(() => {
-    if (scene === "last") {
-      //const existingUser = localStorage.getItem('user')
-      //const userObj = JSON.parse(existingUser)
-      //userObj.intro = 'true'
-      //localStorage.setItem('user', JSON.stringify(userObj));
-      navigate("/map");
-    }
-  }, [scene]);
-
+    console.log(data);
+  }, [slideData]);
   return (
-    <>
-      {scene === "first" ? (
-        <FirstScene scene={scene} setScene={setScene} />
-      ) : null}
-      {scene === "second" ? (
-        <SecondScene scene={scene} setScene={setScene} />
-      ) : null}
-      {
-        //scene === 'third' ? <ThirdScene scene={scene} setScene = {setScene} /> : null
-      }
-    </>
+    <main className="intro">
+      {data ? (
+        <IntroSlide
+          background={data.image}
+          headline={data.headline}
+          paragraph={data.paragraph}
+          func={setSlideId}
+          slideId={slideId}
+          position={data.position ? data.position : ""}
+        />
+      ) : (
+        <Loader />
+      )}
+    </main>
   );
 };
 
-export default Welcome;
+export default Intro;
