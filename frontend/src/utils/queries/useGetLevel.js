642b950e49a5260e00c46d0a0227baba47c242c8
Kai Uwe <pisezdenekpise@seznam.cz>
2024-04-07 09:55:32 +0200

    Create useSetStyle hook and useGetLevel query

---
+++ frontend/src/utils/queries/useGetLevel.js
@@ -0,0 +1,21 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_LEVEL = gql`
+  query GetLevel($id: ID!) {
+    level(id: $id) {
+      bridgeValues
+      bridgeProperties
+      containerValues
+      containerProperties
+      allowedList
+      deniedList
+      numberOfInputs
+    }
+  }
+`;
+
+export const useGetLevel = (id) => {
+  const { loading, error, data } = useQuery(GET_LEVEL, { variables: { id } });
+
+  return { loading, error, data };
+};
